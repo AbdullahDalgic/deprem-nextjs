c70aa9760cab067063ece103709e006819a03232
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-09-30 23:29:56 +0300

    refactor: Update to developers page

--- src/app/gelistiriciler/page.tsx
+++ src/app/gelistiriciler/page.tsx
@@ -1,6 +1,8 @@
-import { SITE_URL } from "@/utils/constants";
+import SectionTitle from "@/components/elements/SectionTitle";
+import Breadcrumb from "@/components/theme/Breadcrumb";
+import { EMAIL, SITE_URL } from "@/utils/constants";
 import SeoData from "@/utils/helpers/seo";
-import { Box, Container } from "@mui/material";
+import { Grid2, Container, Typography, Box } from "@mui/material";
 
 export const generateMetadata = async () => {
   return SeoData({
@@ -10,19 +12,37 @@ export const generateMetadata = async () => {
   });
 };
 
-export default function TermsOfUse() {
+export default function PrivacyPolicy() {
   return (
     <>
-      <Container maxWidth="lg">
-        <Box
-          display={"flex"}
-          gap={2}
-          flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          sx={{ minHeight: "80vh" }}
-        >
-          Bu sayfa yapım aşamasındadır.
+      <Breadcrumb
+        breadcrumbPostTitle="Geliştiriciler"
+        breadcrumbPostUrl="/gelistiriciler"
+      />
+
+      <Container maxWidth="lg" sx={{ mt: "2rem", mb: "4rem" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <Typography variant="h3" component="h1">
+            Geliştiriciler Hakkında
+          </Typography>
+          <Typography variant="body1">
+            <strong>Yürürlük Tarihi:</strong> 30 Eylül 2024
+          </Typography>
+
+          <Typography variant="body1">
+            Açık kaynak yazılımlar, teknoloji dünyasında birlikte çalışmanın ve
+            bilgi paylaşımının en önemli örneklerinden biridir. Bu projeye
+            destek vermek isteyen geliştiriciler, katkıda bulunarak deprem
+            bilgilerini daha erişilebilir hale getirmek için büyük bir fırsata
+            sahiptir.
+          </Typography>
+
+          <Typography variant="body1">
+            Deprem verilerine erişmek isteyen geliştiriciler, API ve webhook
+            talepleri ile bizimle iletişime geçebilir. Bu sayede, kendi
+            uygulamalarında bu değerli bilgileri kullanabilirler. Birlikte, daha
+            güvenli bir toplum oluşturmak için iş birliği yapalım!
+          </Typography>
         </Box>
       </Container>
     </>
