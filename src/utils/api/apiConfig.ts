0ef6126a872e4fe2b3a2db5f73cb43f2c0d48519
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-10-03 09:09:08 +0300

    refactor: Remove unused "Cache-Control" header in API configuration

--- src/utils/api/apiConfig.ts
+++ src/utils/api/apiConfig.ts
@@ -4,9 +4,6 @@ import { API_URL } from "../constants";
 const API = xior.create({
   baseURL: API_URL + "/api",
   timeout: 60000,
-  headers: {
-    "Cache-Control": "max-age=10",
-  },
 });
 
 // API Request interceptor
