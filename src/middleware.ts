ac47952b97c1b307ac0d633432dc420784220524
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-10-03 09:33:46 +0300

    refactor: Update middleware to handle additional URL paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,10 @@ import API from "./utils/api/apiConfig";
 import dayjs from "dayjs";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/deprem/")) {
-    const id = request.nextUrl.pathname.split("/")[2];
-    if (id) {
+  const { pathname } = request.nextUrl || {};
+  if (pathname.startsWith("/deprem/") || pathname.startsWith("/depremler/")) {
+    const id = pathname.split("/")[2];
+    if (id == Number(id).toString()) {
       const { data } = await API.get(`/earthquakes/${id}`);
       if (data) {
         const folder = dayjs(data.eventDate).format("YYYY-MM-DD");
@@ -16,12 +17,12 @@ export async function middleware(request: NextRequest) {
     }
   }
 
-  if (request.nextUrl.pathname.startsWith("/kurumsal/gizlilik-sozlesmesi")) {
+  if (pathname.startsWith("/kurumsal/gizlilik-sozlesmesi")) {
     const url = "/sayfalar/gizlilik-politikasi";
     return NextResponse.redirect(new URL(url, request.url), 302);
   }
 
-  if (request.nextUrl.pathname === "/eklenti") {
+  if (pathname === "/eklenti") {
     return NextResponse.redirect(new URL("/apps", request.url), 302);
   }
 }
