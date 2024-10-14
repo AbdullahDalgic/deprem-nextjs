2271f01666d3c075567198c8844326f9cd34d99c
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-10-14 07:52:53 +0300

    refactor: Add device information to request headers in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import API from "./utils/api/apiConfig";
-import dayjs from "dayjs";
 import { generateEarthquakeLink } from "./utils/helpers/urls";
+import UAParser from "ua-parser-js";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl || {};
+
+  if (!pathname.startsWith("/_next/") && !pathname.startsWith("/api/")) {
+    try {
+      const userAgent = request.headers.get("user-agent") || "";
+      const parser = new UAParser(userAgent);
+      const result = parser.getResult();
+
+      if (result) {
+        return NextResponse.next({
+          ...request,
+          headers: {
+            ...request.headers,
+            "x-device": result.device.model || "Unknown",
+            "x-device_type": result.device.type || "Unknown",
+          },
+        });
+      }
+    } catch (error) {}
+  }
+
   if (pathname.startsWith("/deprem/") || pathname.startsWith("/depremler/")) {
     const id = pathname.split("/")[2];
     if (id == Number(id).toString()) {
