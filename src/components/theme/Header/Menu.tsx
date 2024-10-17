2dfe576b8531161e56e33764929f5d68b172380b
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-10-17 21:17:00 +0300

    title property name changed

--- src/components/theme/Header/Menu.tsx
+++ src/components/theme/Header/Menu.tsx
@@ -109,7 +109,7 @@ export default function Menu({
         <div
           className="mobile-nav-toggler"
           onClick={handleMobileMenuOpen}
-          aria-label="Open main navigation"
+          title="Open main navigation"
         >
           <i className="fas fa-bars" />
         </div>
