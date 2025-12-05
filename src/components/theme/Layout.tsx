04f5b8c5a1f007bf1c0b9dbd7907e00235f86140
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2025-12-05 14:39:01 +0200

    move Google Analytics and Facebook Pixel scripts from Layout component to Scripts component for better organization

--- src/components/theme/Layout.tsx
+++ src/components/theme/Layout.tsx
@@ -166,44 +166,6 @@ export default function Layout({
       />
 
       <Scripts />
-
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-NX6P3CKR55"
-        strategy="lazyOnload"
-        id="google-analytics-script"
-      />
-      <Script strategy="lazyOnload" id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-NX6P3CKR55');
-          console.log('loaded google analytics');
-        `}
-      </Script>
-
-      <Script strategy="lazyOnload" id="pixel">
-        {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '526062079923527');
-        fbq('track', 'PageView');
-        `}
-      </Script>
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=526062079923527&ev=PageView&noscript=1"
-        />
-      </noscript>
     </ThemeProvider>
   );
 }
